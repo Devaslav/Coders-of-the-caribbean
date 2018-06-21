cc41ad9d40914a6d07a237ed449bd7a36a919e7d
Деваслав <@DEVASLAV_PC>
2018-06-22 02:44:40 +0300

    Begin2

--- first_step.c
+++ first_step.c
@@ -15,7 +15,7 @@
 
 #define GET_NUM(x,y,w) (x+w*y)
 
-#define MAX_STEPS	13
+#define MAX_STEPS	10
 
 // STRUCTS
 struct WALKER
@@ -38,8 +38,8 @@ struct DOT
 	int num;
 };
 
-struct DOT DOTS[24 * 20];
-MOVES[24][20];
+struct DOT DOTS[26 * 20];
+int MOVES[20][26];
 
 
 // FUNCTIONS
@@ -47,7 +47,7 @@ MOVES[24][20];
 void CHANGE(int *A, int *B);
 
 // PROGS
-void A_STAR(int x, int y, int w, int h, int STEP);	  		// Алгоритм поиска пути А*
+void A_STAR(int x, int y, int w, int h, int TYPE, int STEP);	  		// Алгоритм поиска пути А*
 char IN_ZONE_X(int x, int w);					  		// Проверка корректности Х координаты
 char IN_ZONE_Y(int y, int h);					  		// Проверка корректности Y координаты
 char VALID_DOT(int x, int y, int w, int h);	  		// Проверка возможности хода
@@ -83,6 +83,15 @@ int main()
 	scanf("%d", &height); fgetc(stdin);
 
 	fprintf(stderr, "w=%d, h=%d \n", width, height);
+	/*
+	char line[5][11] =
+	{
+	'x','x','x','x','x','x','x','x','x','x','x',
+	'.','.','.','.','.','.','.','.','.','.','.',
+	'.','.','.','.','.','.','.','.','.','.','.',
+	'x','x','x','x','x','x','x','x','x','x','x',
+	};
+	*/
 	char line[20][26];
 
 	for (int i = 0; i < height; i++) {
@@ -108,7 +117,6 @@ int main()
 	}
 
 
-
 	int sanityLossLonely; // how much sanity you lose every turn when alone, always 3 until wood 1
 	int sanityLossGroup; // how much sanity you lose every turn when near another player, always 1 until wood 1
 	int wandererSpawnTime; // how many turns the wanderer take to spawn, always 3 until wood 1
@@ -119,7 +127,14 @@ int main()
 	while (1) {
 		int entityCount; // the first given entity corresponds to your explorer
 		scanf("%d", &entityCount);
-		for (int i = 0; i < entityCount; i++) {
+
+		//for (int k = 0; k < entityCount; k++) 
+		for (int i = 0; i < height; i++)
+			for (int j = 0; j < width; j++)
+				MOVES[i][j] = 0;
+
+		for (int i = 0; i < entityCount; i++)
+		{
 			char entityType[21];
 			int id;
 			int x;
@@ -128,12 +143,72 @@ int main()
 			int param1;
 			int param2;
 			scanf("%s%d%d%d%d%d%d", entityType, &id, &x, &y, &param0, &param1, &param2);
+
+			if (entityType[0] == 'E')
+			{
+				A_STAR(x, y, width, height, 0, 0);
+
+				for (int i = 0; i < height; i++)
+					for (int j = 0; j < width; j++)
+					{
+						if (DOTS[GET_NUM(j, i, width)].mass != -1) MOVES[i][j] = MOVES[i][j] + MAX_STEPS - DOTS[GET_NUM(j, i, width)].mass;
+						DOTS[i * width + j].mass = -1;
+					}
+			}
+
+			if (entityType[0] == 'W')
+			{
+				A_STAR(x, y, width, height, 1, 0);
+
+				for (int i = 0; i < height; i++)
+					for (int j = 0; j < width; j++)
+					{
+						if (DOTS[GET_NUM(j, i, width)].mass != -1) MOVES[i][j] = MOVES[i][j] - 5 * (MAX_STEPS - DOTS[GET_NUM(j, i, width)].mass);
+						DOTS[i * width + j].mass = -1;
+					}
+			}
+			//if(entityType[0] == 'W') A_STAR(x, y, width, height, 1, 0);	 
 		}
+
+		/*
+		for (int i = 0; i < height; i++)
+		{
+		fprintf(stderr, "%02d :",i);
+		for (int j = 0; j < width; j++)
+		{
+		fprintf(stderr, " %02d ", DOTS[i * width + j].mass);
+		}
+		fprintf(stderr, "\n");
+		}
+		*/
+		fprintf(stderr, "\n---------\n");
+
+		for (int i = 0; i < height; i++)
+		{
+			fprintf(stderr, "%02d :", i);
+			for (int j = 0; j < width; j++)
+			{
+				fprintf(stderr, " %03d ", MOVES[i][j]);
+			}
+			fprintf(stderr, "\n");
+		}
+
+		Trg_Mass = -1000;
+		for (int i = 0; i < height; i++)
+			for (int j = 0; j < width; j++)
+			{
+				if (Trg_Mass< MOVES[i][j])
+				{
+					Trg_X = j;
+					Trg_Y = i;
+					Trg_Mass = MOVES[i][j];
+				}
+			}
 
 		// Write an action using printf(). DON'T FORGET THE TRAILING \n
 		// To debug: fprintf(stderr, "Debug messages...\n");
 
-		printf("WAIT\n"); // MOVE <x> <y> | WAIT
+		printf("MOVE %d %d\n", Trg_X, Trg_Y); // MOVE <x> <y> | WAIT
 
 	}
 
@@ -177,7 +252,7 @@ char IN_ZONE_Y(int y, int h)
 //////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-void A_STAR(int x, int y, int w, int h, int STEP)
+void A_STAR(int x, int y, int w, int h, int TYPE, int STEP)
 {
 	if (STEP >= MAX_STEPS) return;
 
@@ -186,16 +261,19 @@ void A_STAR(int x, int y, int w, int h, int STEP)
 		if ((DOTS[GET_NUM(x, y, w)].mass == -1) || (DOTS[GET_NUM(x, y, w)].mass > STEP))
 		{
 			DOTS[GET_NUM(x, y, w)].mass = STEP;
-			MOVES[x][y] = DOTS[GET_NUM(x, y, w)].mass;
+
+			//if(TYPE==0)MOVES[x][y] += (MAX_STEPS-DOTS[GET_NUM(x, y, w)].mass); //EXPLORER
+			//if(TYPE==1)MOVES[x][y] -= (MAX_STEPS-DOTS[GET_NUM(x, y, w)].mass); // W-SPAWN
+
 			DOTS[GET_NUM(x, y, w)].num = GET_NUM(x, y, w);
-			A_STAR(x, y, w, h, STEP);
+			A_STAR(x, y, w, h, TYPE, STEP);
 		}
 		else
 		{
-			if (VALID_DOT(x + 1, y, w, h)) 	A_STAR(x + 1, y, w, h, STEP + 1);
-			if (VALID_DOT(x - 1, y, w, h)) 	A_STAR(x - 1, y, w, h, STEP + 1);
-			if (VALID_DOT(x, y + 1, w, h))	A_STAR(x, y + 1, w, h, STEP + 1);
-			if (VALID_DOT(x, y - 1, w, h))	A_STAR(x, y - 1, w, h, STEP + 1);
+			if (VALID_DOT(x + 1, y, w, h)) 	A_STAR(x + 1, y, w, h, TYPE, STEP + 1);
+			if (VALID_DOT(x - 1, y, w, h)) 	A_STAR(x - 1, y, w, h, TYPE, STEP + 1);
+			if (VALID_DOT(x, y + 1, w, h))	A_STAR(x, y + 1, w, h, TYPE, STEP + 1);
+			if (VALID_DOT(x, y - 1, w, h))	A_STAR(x, y - 1, w, h, TYPE, STEP + 1);
 		}
 	}
 	else return;
@@ -208,7 +286,7 @@ char VALID_DOT(int x, int y, int w, int h)
 	if
 		(
 			!IN_ZONE_X(x, w) || !IN_ZONE_Y(y, h) ||
-			(DOTS[GET_NUM(x, y, w)].SYMB != '.')
+			(DOTS[GET_NUM(x, y, w)].SYMB != '.' && DOTS[GET_NUM(x, y, w)].SYMB != 'w')
 			//||IN_BOMBS(x,y, B_NUM)
 			//|| LOCKED_DOT(x,y)
 			)
